0d07bb9773d50d2a517510ce107a5c1284fcb9d1
mac2UZaM583 <163586801+mac2UZaM583@users.noreply.github.com>
2024-09-07 18:29:50 +0300

    Delete src/urls.rs

--- src/urls.rs
+++
@@ -1,8 +0,0 @@
-pub const DOMEN: &str = "https://api";
-
-pub const TICKERS: &str = ".bybit.com/v5/market/tickers?category=linear";
-pub const INSTRUMENTS_INFO: &str = ".bybit.com/v5/market/instruments-info?category=linear&symbol=";
-pub const WALLET_BALANCE: &str = ".bybit.com/v5/account/wallet-balance";
-
-// demo
-// https://api-demo.bybit.com/
